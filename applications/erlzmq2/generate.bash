af2a8c517793d999a8f1a3921845d8d48b51be10
Ciprian Dorin, Craciun <ciprian@volution.ro>
2012-05-16 23:56:32 +0300

    Update `generate` script

--- applications/erlzmq2/generate.bash
+++ applications/erlzmq2/generate.bash
@@ -12,11 +12,14 @@ cp -T ./repositories/erlzmq2/src/erlzmq.app.src ./.generated/erlzmq.app
 
 gcc -shared -o ./.generated/erlzmq_drv.so \
 		-I ./repositories/erlzmq2/c_src \
-		-I /usr/lib/erlang/usr/include \
 		-I "${mosaic_pkg_zeromq:-/usr}/include" \
+		-I "${mosaic_pkg_erlang:-/usr/lib/erlang}/usr/include" \
+		-L "${mosaic_pkg_erlang:-/usr/lib/erlang}/usr/lib" \
+		${mosaic_CFLAGS:-} ${mosaic_LDFLAGS:-} \
 		./repositories/erlzmq2/c_src/erlzmq_nif.c \
 		./repositories/erlzmq2/c_src/vector.c \
 		"${mosaic_pkg_zeromq:-/usr}/lib/libzmq.a" \
-		-lstdc++ -luuid
+		-lstdc++ -luuid \
+		${mosaic_LIBS:-}
 
 exit 0
