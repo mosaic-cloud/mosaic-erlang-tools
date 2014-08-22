6c6ad00aed54da76e34c6f9d11085fa9d9db0a33
Ciprian Dorin Craciun <ciprian@volution.ro>
2014-08-22 19:02:19 +0300

    Import cowboy application and its dependencies

---
+++ applications/cowlib/generate.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e -E -u -o pipefail -o noclobber -o noglob -o braceexpand || exit 1
+trap 'printf "[ee] failed: %s\n" "${BASH_COMMAND}" >&2' ERR || exit 1
+
+test "${#}" -eq 0
+
+cd -- "$( dirname -- "$( readlink -e -- "${0}" )" )"
+test -d "${_generate_outputs}"
+
+cp -T ./repositories/cowlib/src/cowlib.app.src "${_generate_outputs}/cowlib.app"
+
+exit 0
