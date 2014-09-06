d1e9707299dd4c6ce5a1fc5142790d85f9ad3145
Ciprian Dorin Craciun <ciprian@volution.ro>
2014-09-06 12:08:38 +0300

    Add support for `pallur_pkg_*` variables and `_env-0.bash`

--- scripts/_env.bash
+++ scripts/_env.bash
@@ -15,6 +15,10 @@ _PATH="${pallur_PATH:-${_tools}/bin:${PATH}}"
 _HOME="${pallur_HOME:-${HOME}}"
 _TMPDIR="${pallur_TMPDIR:-${TMPDIR:-${_temporary}}}"
 
+if test -e "${_scripts}/_env-0.bash" ; then
+	. -- "${_scripts}/_env-0.bash"
+fi
+
 if test -n "${pallur_pkg_erlang:-}" ; then
 	_erl_bin="${pallur_pkg_erlang}/bin/erl"
 elif test -e "${_tools}/pkg/erlang" ; then
@@ -51,16 +55,32 @@ if test -z "${_dialyzer_bin}" ; then
 	_dialyzer_bin=false
 fi
 
-_vbs_bin="$( PATH="${_PATH}" type -P -- vbs || true )"
+if test -n "${pallur_pkg_vbs:-}" ; then
+	_vbs_bin="${pallur_pkg_vbs}/bin/vbs"
+elif test -e "${_tools}/pkg/vbs" ; then
+	_vbs_bin="${_tools}/pkg/vbs/bin/vbs"
+else
+	_vbs_bin="$( PATH="${_PATH}" type -P -- vbs || true )"
+fi
 if test -z "${_vbs_bin}" ; then
 	echo "[ee] missing \`vbs\` (Volution Build System tool) executable in path: \`${_PATH}\`; ignoring!" >&2
 	_vbs_bin=false
 fi
 
-_ninja_bin="$( PATH="${_PATH}" type -P -- ninja || true )"
+if test -n "${pallur_pkg_ninja:-}" ; then
+	_ninja_bin="${pallur_pkg_ninja}/bin/ninja"
+elif test -n "${_tools}/pkg/ninja" ; then
+	_ninja_bin="${_tools}/pkg/ninja/bin/ninja"
+else
+	_ninja_bin="$( PATH="${_PATH}" type -P -- ninja || true )"
+fi
 if test -z "${_ninja_bin}" ; then
 	echo "[ee] missing \`ninja\` (Ninja build tool) executable in path: \`${_PATH}\`; ignoring!" >&2
 	_ninja_bin=false
+fi
+
+if test -n "${pallur_pkg_erlang:-}" ; then
+	_PATH="${pallur_pkg_erlang}/bin:${_PATH}"
 fi
 
 _generic_env=(
@@ -69,12 +89,15 @@ _generic_env=(
 		TMPDIR="${_TMPDIR}"
 )
 
+# FIXME: This should be handled in a generic way!
 if test -n "${pallur_pkg_erlang:-}" ; then _generic_env+=( pallur_pkg_erlang="${pallur_pkg_erlang}" ) ;
 elif test -e "${_tools}/pkg/erlang" ; then _generic_env+=( pallur_pkg_erlang="${_tools}/pkg/erlang" ) ; fi
 
+# FIXME: This should be handled in a generic way!
 if test -n "${pallur_pkg_zeromq:-}" ; then _generic_env+=( pallur_pkg_zeromq="${pallur_pkg_zeromq}" ) ;
 elif test -e "${_tools}/pkg/zeromq" ; then _generic_env+=( pallur_pkg_zeromq="${_tools}/pkg/zeromq" ) ; fi
 
+# FIXME: This should be handled in a generic way!
 if test -n "${pallur_pkg_jansson:-}" ; then _generic_env+=( pallur_pkg_jansson="${pallur_pkg_jansson}" ) ;
 elif test -e "${_tools}/pkg/jansson" ; then _generic_env+=( pallur_pkg_jansson="${_tools}/pkg/jansson" ) ; fi
 
@@ -133,6 +156,13 @@ _vbs_env=(
 		_temporary="${_temporary}"
 		_outputs="${_outputs}"
 		_generated="${_generated}"
+		vbs_executables__erl__path="${_erl_bin}"
+		vbs_executables__erl__version="${_otp_version}"
+		vbs_executables__gcc__path="$( PATH="${_PATH}" type -P -- gcc )"
+		vbs_executables__cp__path="$( PATH="${_PATH}" type -P -- cp )"
+		vbs_executables__mkdir__path="$( PATH="${_PATH}" type -P -- mkdir )"
+		vbs_executables__touch__path="$( PATH="${_PATH}" type -P -- touch )"
+		vbs_executables__zip__path="$( PATH="${_PATH}" type -P -- zip )"
 )
 
 _ninja_file="${_outputs}/.make.ninja"
